30ddc5f0aa0f435a2b339b180b5ddb69c1776a81
Thiruisa <thiruisa@homtail.co.uk>
2023-12-17 19:32:53 +0000

    header

--- space-project/src/components/Header.js
+++ space-project/src/components/Header.js
@@ -5,13 +5,53 @@ import logo from "../starter-code/assets/shared/logo.svg"
 export default function Header() {
     return (
         <>
-            <header>
-                <div>
-                    <NavLink to="/">
-                        <img src={logo} alt="" title=""/>
-                    </NavLink>
-                </div>
-            </header>
+          <header>
+            <div>
+              <NavLink to="/">
+                <img src={logo} alt="space" title="Travel to Space" />
+              </NavLink>
+            </div>
+    
+            <nav>
+              <ul>
+                <li>
+                  <NavLink
+                    to="/"
+                    exact>
+                    <span>00</span> Home
+                  </NavLink>
+                </li>
+                <li>
+                  <NavLink
+                    to="/destination"
+                    exact>
+                    <span>01</span> Destination
+                  </NavLink>
+                </li>
+                <li>
+                  <NavLink
+                    to="/crew"
+                    exact>
+                    <span>02</span> Crew
+                  </NavLink>
+                </li>
+                <li>
+                  <NavLink
+                    to="/technology"
+                    exact>
+                    <span>03</span> Technology
+                  </NavLink>
+                </li>
+              </ul>
+            </nav>
+            {/* )} */}
+    
+            {/* <div className="menu">
+              <button id="menu" onClick={handleClick}>
+                <FiMenu className="text-white text-4xl" />
+              </button>
+            </div> */}
+          </header>
         </>
-    )
+      )
 }
--- space-project/src/components/index.js
+++ space-project/src/components/index.js
@@ -1,7 +1,7 @@
-import Header from "./Header"
-import Home from "./Home"
-import Destination from "./Destination"
-import Crew from "./Crew"
-import Technology from "./Technology"
+import Header from "./Header";
+import Home from "./Home";
+import Destination from "./Destination";
+import Crew from "./Crew";
+import Technology from "./Technology";
 
 export { Header, Home, Destination, Crew, Technology }
